1be694b7c695156a9b6b0b53ce9f372fe9632fcb
Anthony Soulain <anthony.soulain@gmail.com>
2020-04-09 16:09:16 +1000

    Minor change (no capitalize if not in Simbad).

--- previs/core.py
+++ previs/core.py
@@ -42,7 +42,7 @@ from astroquery.vizier import Vizier
 from previs.instr import (chara_limit, gravity_limit, matisse_limit,
                           pionier_limit)
 from previs.sed import getSed, sed2mag
-from previs.utils import printtime, check_servers_response
+from previs.utils import check_servers_response, printtime
 from termcolor import cprint
 from uncertainties import ufloat
 
@@ -85,8 +85,8 @@ def search(star, source='ESO', check=False, verbose=True):
         return None
 
     start_time = time.time()
+    star_user = star
     star = star.upper()
-
     if verbose:
         cprint('\n%s: search started (could take up to 30 seconds)...' %
                star, 'cyan')
@@ -122,7 +122,7 @@ def search(star, source='ESO', check=False, verbose=True):
         pass
 
     if not data['Simbad']:
-        cprint('## Error: %s not in Simbad!' % star, 'red')
+        cprint('## Problem: %s not in Simbad!' % star_user, 'red')
         return data
     # --------------------------------------
     #                 SED
@@ -258,7 +258,6 @@ def search(star, source='ESO', check=False, verbose=True):
     Obs = {'VLTI': False, 'CHARA': False}
 
     min_elev = 40
-
     if c.dec.deg <= ((90 - min_elev) - abs(L_paranal)):
         Obs['VLTI'] = True
     if c.dec.deg >= (L_chara - (90 - min_elev)):
@@ -278,6 +277,7 @@ def search(star, source='ESO', check=False, verbose=True):
     data['Name'] = star
     if verbose:
         printtime('Check Instruments: done', t3)
+    cprint('Done (%2.2f s).' % (time.time()-start_time), 'cyan')
     return data
 
 
@@ -295,7 +295,7 @@ def survey(list_star):
     """
     if check_servers_response() is None:
         return None
-   
+
     cprint('\nStarting survey on %i stars:' % len(list_star), 'cyan')
     cprint('-------------------------', 'cyan')
     out = {}
