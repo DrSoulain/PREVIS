c397f8839bfa5ac4bcc6477683d0edba3fda26b2
Anthony <anthony.soulain@protonmail.com>
2023-02-02 17:23:01 +0100

    black formating

--- previs/core.py
+++ previs/core.py
@@ -38,19 +38,23 @@ import numpy as np
 from astropy import units as u
 from astroquery.simbad import Simbad
 from astroquery.vizier import Vizier
-from previs.instr import (chara_limit, gravity_limit, matisse_limit,
-                          pionier_limit)
+from previs.instr import (
+    chara_limit,
+    gravity_limit,
+    ivis_limit,
+    matisse_limit,
+    pionier_limit,
+)
 from previs.sed import getSed, sed2mag
 from previs.utils import check_servers_response, printtime
 from termcolor import cprint
-from tqdm import tqdm
 from uncertainties import ufloat
 
-warnings.filterwarnings('ignore')
-warnings.filterwarnings('ignore', module='scipy.interpolate.interp1d')
+warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", module="scipy.interpolate.interp1d")
 
 
-def search(star, source='ESO', check=False, verbose=True):
+def search(star, source="ESO", check=False, verbose=False):
     """ Perform a large search to get informations about a star or a list of stars (observability, magnitude, distance, sed, etc.)
 
     Parameters
@@ -89,168 +93,180 @@ def search(star, source='ESO', check=False, verbose=True):
 
     start_time = time.time()
     if type(star) != str:
-        cprint("Error: input need to be a target name (str format).", 'red')
+        if verbose:
+            cprint("Error: input need to be a target name (str format).", "red")
         return None
 
     star_user = star
     star = star.upper()
     if verbose:
-        cprint('\n%s: search started (could take up to 30 seconds)...' %
-               star, 'cyan')
-    data = {'Simbad': False}
-    data['Ins'] = None
-    data['Name'] = star
+        cprint("\n%s: search started (could take up to 30 seconds)..." % star, "cyan")
+    data = {"Simbad": False}
+    data["Ins"] = None
+    data["Name"] = star
     # --------------------------------------
     #       Coordinates of the target
     # --------------------------------------
     coord = {}
     customSimbad = Simbad()
-    customSimbad.add_votable_fields('parallax', 'sptype', 'id',
-                                    'flux(V)', 'flux(B)')
+    customSimbad.add_votable_fields("parallax", "sptype", "id", "flux(V)", "flux(B)")
 
     try:
         objet = customSimbad.query_object(star)
-        coord[star] = np.array([objet['RA'][0], objet['DEC'][0]])
-        coordo = str(coord[star][0]) + ' ' + str(coord[star][1])
+        coord[star] = np.array([objet["RA"][0], objet["DEC"][0]])
+        coordo = str(coord[star][0]) + " " + str(coord[star][1])
         c = ac.SkyCoord(coordo, unit=(u.hourangle, u.deg))
-        data['Coord'] = coordo
+        data["Coord"] = coordo
 
-        plx = ufloat(objet['PLX_VALUE'].data[0],
-                     objet['PLX_ERROR'].data[0])
-        d = 1/plx
-        data['Distance'] = {'d': d.nominal_value, 'e_d': d.std_dev}
-        data['Simbad'] = True
-        aa = str(objet['SP_TYPE'].data[0])
-        sptype = aa.split('b')[1].split("'")[1]
-        data['Sp_type'] = sptype
+        plx = ufloat(objet["PLX_VALUE"].data[0], objet["PLX_ERROR"].data[0])
+        d = 1 / plx
+        data["Distance"] = {"d": d.nominal_value, "e_d": d.std_dev}
+        data["Simbad"] = True
+        aa = str(objet["SP_TYPE"].data[0])
+        sptype = aa.split("b")[1].split("'")[1]
+        data["Sp_type"] = sptype
     except Exception:
         pass
-
-    if not data['Simbad']:
-        cprint('## Error: %s not in Simbad!' % star_user, 'red')
+    if not data["Simbad"]:
+        if verbose:
+            cprint("## Error: %s not in Simbad!" % star_user, "red")
         return data
     # --------------------------------------
     #                 SED
     # --------------------------------------
     if verbose:
-        print('Get SED from Vizier database...')
+        print("Get SED from Vizier database...")
     sed = getSed(coordo)
-    data['SED'] = sed
+    data["SED"] = sed
 
     # --------------------------------------
     #       Magnitude of the target
     # --------------------------------------
-    l_bands = ['B', 'V', 'R', 'J', 'H', 'K', 'L', 'M', 'N']
+    l_bands = ["B", "V", "R", "J", "H", "K", "L", "M", "N"]
 
-    with np.errstate(divide='ignore'):
-        magB, magV, magR, magJ, magH, magK, magL, magM, magN = sed2mag(
-            sed, l_bands)
+    try:
+        with np.errstate(divide="ignore"):
+            magB, magV, magR, magJ, magH, magK, magL, magM, magN = sed2mag(sed, l_bands)
+    except TypeError:
+        return None
 
     if np.isnan(magV):
         try:
-            magV = objet['FLUX_V'][0]
+            magV = objet["FLUX_V"][0]
             if magV is np.ma.masked:
                 magV = np.nan
         except Exception:
             pass
     try:
-        magB = objet['FLUX_B'][0]
+        magB = objet["FLUX_B"][0]
         if magB is np.ma.masked:
             magB = np.nan
     except Exception:
         magB = np.nan
 
-    data['Mag'] = {'magB': float(magB),
-                   'magV': float(magV),
-                   'magR': float(magR),
-                   'magH': float(magH),
-                   'magK': float(magK),
-                   'magL': float(magL),
-                   'magM': float(magM),
-                   'magN': float(magN),
-                   'magJ': float(magJ)
-                   }
+    data["Mag"] = {
+        "magB": float(magB),
+        "magV": float(magV),
+        "magR": float(magR),
+        "magH": float(magH),
+        "magK": float(magK),
+        "magL": float(magL),
+        "magM": float(magM),
+        "magN": float(magN),
+        "magJ": float(magJ),
+    }
 
     if verbose:
-        t1 = printtime('Check SED: done', start_time)
+        t1 = printtime("Check SED: done", start_time)
     # --------------------------------------
     #                GAIA DR2
     # --------------------------------------
-    columns = ['_r', 'RA_ICRS', 'DE_ICRS', 'e_RA_ICRS', 'e_DE_ICRS',
-               'Gmag', 'Plx', 'e_Plx', 'pmRA', 'e_pmRA', 'pmDE', 'e_pmDE', 'Teff']
+    columns = [
+        "_r",
+        "RA_ICRS",
+        "DE_ICRS",
+        "e_RA_ICRS",
+        "e_DE_ICRS",
+        "Gmag",
+        "Plx",
+        "e_Plx",
+        "pmRA",
+        "e_pmRA",
+        "pmDE",
+        "e_pmDE",
+        "Teff",
+    ]
     v = Vizier(columns=columns)
-    data['Gaia_dr2'] = {}
+    data["Gaia_dr2"] = {}
     try:
-        res = v.query_region(star, radius="2s", catalog='I/345/gaia2')
-        data['Mag']['magG'] = float(
-            np.ma.getdata(res['I/345/gaia2']['Gmag'])[0])
-        data['Gaia_dr2']['RA'] = float(
-            np.ma.getdata(res['I/345/gaia2']['RA_ICRS'])[0])
-        data['Gaia_dr2']['e_RA'] = float(
-            np.ma.getdata(res['I/345/gaia2']['e_RA_ICRS'])[0])
-        data['Gaia_dr2']['DEC'] = float(
-            np.ma.getdata(res['I/345/gaia2']['DE_ICRS'])[0])
-        data['Gaia_dr2']['e_DEC'] = float(
-            np.ma.getdata(res['I/345/gaia2']['e_DE_ICRS'])[0])
-        data['Gaia_dr2']['Plx'] = float(
-            np.ma.getdata(res['I/345/gaia2']['Plx'])[0])
-        data['Gaia_dr2']['e_Plx'] = float(
-            np.ma.getdata(res['I/345/gaia2']['e_Plx'])[0])
-        data['Gaia_dr2']['pmRA'] = float(
-            np.ma.getdata(res['I/345/gaia2']['pmRA'])[0])
-        data['Gaia_dr2']['e_pmRA'] = float(
-            np.ma.getdata(res['I/345/gaia2']['e_pmRA'])[0])
-        data['Gaia_dr2']['pmDE'] = float(
-            np.ma.getdata(res['I/345/gaia2']['pmDE'])[0])
-        data['Gaia_dr2']['e_pmDE'] = float(
-            np.ma.getdata(res['I/345/gaia2']['e_pmDE'])[0])
-        data['Gaia_dr2']['Teff'] = float(
-            np.ma.getdata(res['I/345/gaia2']['Teff'])[0])
+        res = v.query_region(star, radius="2s", catalog="I/345/gaia2")
+        data["Mag"]["magG"] = float(np.ma.getdata(res["I/345/gaia2"]["Gmag"])[0])
+        data["Gaia_dr2"]["RA"] = float(np.ma.getdata(res["I/345/gaia2"]["RA_ICRS"])[0])
+        data["Gaia_dr2"]["e_RA"] = float(
+            np.ma.getdata(res["I/345/gaia2"]["e_RA_ICRS"])[0]
+        )
+        data["Gaia_dr2"]["DEC"] = float(np.ma.getdata(res["I/345/gaia2"]["DE_ICRS"])[0])
+        data["Gaia_dr2"]["e_DEC"] = float(
+            np.ma.getdata(res["I/345/gaia2"]["e_DE_ICRS"])[0]
+        )
+        data["Gaia_dr2"]["Plx"] = float(np.ma.getdata(res["I/345/gaia2"]["Plx"])[0])
+        data["Gaia_dr2"]["e_Plx"] = float(np.ma.getdata(res["I/345/gaia2"]["e_Plx"])[0])
+        data["Gaia_dr2"]["pmRA"] = float(np.ma.getdata(res["I/345/gaia2"]["pmRA"])[0])
+        data["Gaia_dr2"]["e_pmRA"] = float(
+            np.ma.getdata(res["I/345/gaia2"]["e_pmRA"])[0]
+        )
+        data["Gaia_dr2"]["pmDE"] = float(np.ma.getdata(res["I/345/gaia2"]["pmDE"])[0])
+        data["Gaia_dr2"]["e_pmDE"] = float(
+            np.ma.getdata(res["I/345/gaia2"]["e_pmDE"])[0]
+        )
+        data["Gaia_dr2"]["Teff"] = float(np.ma.getdata(res["I/345/gaia2"]["Teff"])[0])
 
-        plx = ufloat(data['Gaia_dr2']['Plx'], data['Gaia_dr2']['e_Plx'])
+        plx = ufloat(data["Gaia_dr2"]["Plx"], data["Gaia_dr2"]["e_Plx"])
 
-        Dkpc = 1./plx
-        data['Gaia_dr2']['check'] = True
-        data['Gaia_dr2']['Dkpc'] = Dkpc.nominal_value
-        data['Gaia_dr2']['e_Dkpc'] = Dkpc.std_dev
+        Dkpc = 1.0 / plx
+        data["Gaia_dr2"]["check"] = True
+        data["Gaia_dr2"]["Dkpc"] = Dkpc.nominal_value
+        data["Gaia_dr2"]["e_Dkpc"] = Dkpc.std_dev
     except Exception:
-        data['Gaia_dr2']['check'] = False
-        data['Gaia_dr2']['Dkpc'] = np.nan
-        data['Gaia_dr2']['e_Dkpc'] = np.nan
-        data['Gaia_dr2']['Plx'] = np.nan
-        data['Gaia_dr2']['e_Plx'] = np.nan
-        data['Gaia_dr2']['pmRA'] = np.nan
-        data['Gaia_dr2']['pmDE'] = np.nan
-        data['Mag']['magG'] = np.nan
+        data["Gaia_dr2"]["check"] = False
+        data["Gaia_dr2"]["Dkpc"] = np.nan
+        data["Gaia_dr2"]["e_Dkpc"] = np.nan
+        data["Gaia_dr2"]["Plx"] = np.nan
+        data["Gaia_dr2"]["e_Plx"] = np.nan
+        data["Gaia_dr2"]["pmRA"] = np.nan
+        data["Gaia_dr2"]["pmDE"] = np.nan
+        data["Mag"]["magG"] = np.nan
     if verbose:
-        t2 = printtime('Check Gaia: done', t1)
+        t2 = printtime("Check Gaia: done", t1)
     # --------------------------------------
     #             Guiding star
     # --------------------------------------
-    data['Guiding_star'] = {}
-    v = Vizier(columns=['*', '+<Gmag>'])
+    data["Guiding_star"] = {}
+    v = Vizier(columns=["*", "+<Gmag>"])
 
-    cond_guid_1 = (np.isnan(data['Mag']['magG'])
-                   and (np.isnan(data['Mag']['magR'])))
-    cond_guid_2 = ((data['Mag']['magG'] >= 12.5)
-                   or (data['Mag']['magG'] <= -3))
-    cond_guid_3 = (np.isnan(data['Mag']['magG']) and (
-        (data['Mag']['magR'] >= 12.5) or (data['Mag']['magR'] <= -3)))
+    cond_guid_1 = np.isnan(data["Mag"]["magG"]) and (np.isnan(data["Mag"]["magR"]))
+    cond_guid_2 = (data["Mag"]["magG"] >= 12.5) or (data["Mag"]["magG"] <= -3)
+    cond_guid_3 = np.isnan(data["Mag"]["magG"]) and (
+        (data["Mag"]["magR"] >= 12.5) or (data["Mag"]["magR"] <= -3)
+    )
 
     if cond_guid_1 or cond_guid_2 or cond_guid_3:
-        res = v.query_region(star, radius='57s', catalog='I/337/gaia')
+        res = v.query_region(star, radius="57s", catalog="I/337/gaia")
 
-        Gmag = np.ma.getdata(res['I/337/gaia']['__Gmag_'])
-        cond1 = (Gmag <= 12.5)
+        try:
+            Gmag = np.ma.getdata(res["I/337/gaia"]["__Gmag_"])
+        except TypeError:
+            return None
+        cond1 = Gmag <= 12.5
         cond2 = (Gmag <= 15) & (Gmag > 12.5)
 
-        gmag1 = np.ma.getdata(res['I/337/gaia']['__Gmag_'][cond1])
-        ra1 = np.ma.getdata(res['I/337/gaia']['RA_ICRS'][cond1])
-        dec1 = np.ma.getdata(res['I/337/gaia']['DE_ICRS'][cond1])
+        gmag1 = np.ma.getdata(res["I/337/gaia"]["__Gmag_"][cond1])
+        ra1 = np.ma.getdata(res["I/337/gaia"]["RA_ICRS"][cond1])
+        dec1 = np.ma.getdata(res["I/337/gaia"]["DE_ICRS"][cond1])
 
-        gmag2 = np.ma.getdata(res['I/337/gaia']['__Gmag_'][cond2])
-        ra2 = np.ma.getdata(res['I/337/gaia']['RA_ICRS'][cond2])
-        dec2 = np.ma.getdata(res['I/337/gaia']['DE_ICRS'][cond2])
+        gmag2 = np.ma.getdata(res["I/337/gaia"]["__Gmag_"][cond2])
+        ra2 = np.ma.getdata(res["I/337/gaia"]["RA_ICRS"][cond2])
+        dec2 = np.ma.getdata(res["I/337/gaia"]["DE_ICRS"][cond2])
 
         guid1, guid2 = [], []
 
@@ -259,46 +275,55 @@ def search(star, source='ESO', check=False, verbose=True):
         for i in range(len(ra2)):
             guid2.append([float(ra2[i]), float(dec2[i]), float(gmag2[i])])
 
-        data['Guiding_star']['VLTI'] = [guid1, guid2]
+        data["Guiding_star"]["VLTI"] = [guid1, guid2]
     else:
-        data['Guiding_star']['VLTI'] = 'Science star'
+        data["Guiding_star"]["VLTI"] = "Science star"
 
-    data['Guiding_star']['CHARA'] = bool((np.min([magV, magR]) <= 10))
+    data["Guiding_star"]["CHARA"] = bool((np.min([magV, magR]) <= 10))
 
     if verbose:
-        t3 = printtime('Check guiding star: done,', t2)
+        t3 = printtime("Check guiding star: done,", t2)
     # --------------------------------------
     #             Observability
     # --------------------------------------
     L_paranal = -24.63  # Lattitude deg
     L_chara = 34.2236
-    Obs = {'VLTI': False, 'CHARA': False}
+    Obs = {"VLTI": False, "CHARA": False}
 
     min_elev = 40
     if c.dec.deg <= ((90 - min_elev) - abs(L_paranal)):
-        Obs['VLTI'] = True
+        Obs["VLTI"] = True
     if c.dec.deg >= (L_chara - (90 - min_elev)):
-        Obs['CHARA'] = True
+        Obs["CHARA"] = True
 
-    data['Observability'] = Obs
+    data["Observability"] = Obs
     # --------------------------------------
     #       Limit interferometers
     # --------------------------------------
     tmp = {}
-    tmp['PIONIER'] = pionier_limit(magH)
-    tmp['CHARA'] = chara_limit(magK, magH, magR, magV)
-    tmp['MATISSE'] = matisse_limit(magL, magM, magN, magK,
-                                   source=source, check=check)
-    tmp['GRAVITY'] = gravity_limit(magV, magK)
-    data['Ins'] = tmp
-    data['Name'] = star
+    tmp["PIONIER"] = pionier_limit(magH)
+    tmp["CHARA"] = chara_limit(magK, magH, magR, magV)
+    tmp["MATISSE"] = matisse_limit(magL, magM, magN, magK, source=source, check=check)
+    tmp["GRAVITY"] = gravity_limit(magV, magK)
+    tmp["VISION"] = ivis_limit(magR)
+    data["Ins"] = tmp
+    data["Name"] = star
     if verbose:
-        printtime('Check Instruments: done', t3)
-        cprint('Done (%2.2f s).' % (time.time()-start_time), 'cyan')
+        printtime("Check Instruments: done", t3)
+        cprint("Done (%2.2f s)." % (time.time() - start_time), "cyan")
     return data
 
 
-def survey(list_star, source='ESO', check=False, verbose=False):
+def f(out, star):
+    # try:
+    out[star] = search(star, check=False, verbose=False)
+    # except Exception:
+    #     print(star)
+    #     pass
+    return out
+
+
+def survey(list_star):
     """ Perform previs search on a list of stars.
     Parameters
     ----------
@@ -311,9 +336,14 @@ def survey(list_star, source='ESO', check=False, verbose=False):
     """
     if check_servers_response() is None:
         return None
-    cprint('\nStarting survey on %i stars:' % len(list_star), 'cyan')
-    cprint('-------------------------', 'cyan')
-    out = {}
-    for star in tqdm(list_star, ncols=60, desc="Progress"):
-        out[star] = search(star, source=source, check=check, verbose=verbose)
-    return out
+    cprint("\nStarting survey on %i stars:" % len(list_star), "cyan")
+    cprint("-------------------------", "cyan")
+
+    from multiprocess import Process, Manager
+
+    manager = Manager()
+    d = manager.dict()
+    job = [Process(target=f, args=(d, list_star[i])) for i in range(len(list_star))]
+    _ = [p.start() for p in job]
+    _ = [p.join() for p in job]
+    return d
--- previs/sed.py
+++ previs/sed.py
@@ -18,9 +18,9 @@ from astroquery.vizier import Vizier
 from scipy.constants import c as c_light
 from scipy.interpolate import interp1d
 
-warnings.filterwarnings("ignore", module='astropy.io.votable.tree')
-warnings.filterwarnings("ignore", module='astropy.io.votable.xmlutil')
-warnings.filterwarnings('ignore', module='scipy.interpolate.interp1d')
+warnings.filterwarnings("ignore", module="astropy.io.votable.tree")
+warnings.filterwarnings("ignore", module="astropy.io.votable.xmlutil")
+warnings.filterwarnings("ignore", module="scipy.interpolate.interp1d")
 
 store_directory = Path(__file__).parent / "data"
 
@@ -42,29 +42,31 @@ def getSed(coord):
         'References' (references/publications)):
     """
     try:
-        coord_ = coord.replace(' ', '+').replace('+-', '-')
-        response = urllib.request.urlopen(
-            f'http://vizier.u-strasbg.fr/viz-bin/sed?-c={coord_}&-c.rs=2')
+        coord_ = coord.replace(" ", "+").replace("+-", "-")
+        f = f"http://vizier.u-strasbg.fr/viz-bin/sed?-c={coord_}&-c.rs=1"
+        # f = f"http://vizier.u-strasbg.fr/vizier/sed/?submitSimbad=Photometry&-c={coord_}&-c.r=1&-c.u=arcsec&show_settings=1"
+        response = urllib.request.urlopen(f)
         with tempfile.TemporaryFile() as tmpfile:
             tmpfile.write(response.read())
             tab = np.ma.getdata(vo.parse_single_table(tmpfile).array)
 
-        catalogs = [x.decode('UTF-8') for x in tab['_tabname']]
-        references = getVizierRef(catalogs)
+        catalogs = [x for x in tab["_tabname"]]
+        # references = getVizierRef(catalogs)
 
-        cond = tab['sed_flux'] >= 0
-        freq = tab['sed_freq'][cond]
-        wl = c_light/(freq*1e9)*1e6
-        flux = tab['sed_flux'][cond].astype(float)
-        err = tab['sed_eflux'][cond].astype(float)
+        cond = tab["sed_flux"] >= 0
+        freq = tab["sed_freq"][cond]
+        wl = c_light / (freq * 1e9) * 1e6
+        flux = tab["sed_flux"][cond].astype(float)
+        err = tab["sed_eflux"][cond].astype(float)
 
-        data = {'Flux': list(flux),
-                'Err': list(err),
-                'wl': list(wl),
-                'References': list(references),
-                'Catalogs': list(catalogs)
-                }
-    except urllib.request.HTTPError:
+        data = {
+            "Flux": list(flux),
+            "Err": list(err),
+            "wl": list(wl),
+            "References": [],  # list(references),
+            "Catalogs": list(catalogs),
+        }
+    except (urllib.request.HTTPError, Exception):
         # todo: logme
         data = None
     return data
@@ -75,29 +77,28 @@ def sed2mag(sed, bands):
     Extract magnitude from interpolated SED.
     """
     conv_flux = {
-        'B': {'wl': 0.44, 'F0': 4260},
-        'V': {'wl': 0.5556, 'F0': 3540},  # Allen's astrophysical quantities
-        'R': {'wl': 0.64, 'F0': 3080},
-        'I': {'wl': 0.79, 'F0': 2550},
-        'J': {'wl': 1.215, 'F0': 1630},
-        'H': {'wl': 1.654, 'F0': 1050},
-        'K': {'wl': 2.179, 'F0': 655},
-        'L': {'wl': 3.547, 'F0': 276},
-        'M': {'wl': 4.769, 'F0': 160},
+        "B": {"wl": 0.44, "F0": 4260},
+        "V": {"wl": 0.5556, "F0": 3540},  # Allen's astrophysical quantities
+        "R": {"wl": 0.64, "F0": 3080},
+        "I": {"wl": 0.79, "F0": 2550},
+        "J": {"wl": 1.215, "F0": 1630},
+        "H": {"wl": 1.654, "F0": 1050},
+        "K": {"wl": 2.179, "F0": 655},
+        "L": {"wl": 3.547, "F0": 276},
+        "M": {"wl": 4.769, "F0": 160},
         # 10.2, 42.7 Johnson N (https://www.gemini.edu/?q=node/11119)
-        'N': {'wl': 10.2, 'F0': 42.7},
-        'Q': {'wl': 20.13, 'F0': 9.7},
+        "N": {"wl": 10.2, "F0": 42.7},
+        "Q": {"wl": 20.13, "F0": 9.7},
     }
 
-    with np.errstate(divide='ignore'):
-        f_sed = interp1d(sed['wl'], np.log10(
-            sed['Flux']), bounds_error=False)
+    with np.errstate(divide="ignore"):
+        f_sed = interp1d(sed["wl"], np.log10(sed["Flux"]), bounds_error=False)
 
     l_m = np.full(len(bands), np.nan)
     for i, band in enumerate(bands):
-        F = 10**(f_sed(conv_flux[band]['wl']))
-        F0 = conv_flux[band]['F0']
-        l_m[i] = -2.5*np.log10(F/F0)
+        F = 10 ** (f_sed(conv_flux[band]["wl"]))
+        F0 = conv_flux[band]["F0"]
+        l_m[i] = -2.5 * np.log10(F / F0)
     return list(l_m)
 
 
@@ -116,10 +117,10 @@ def find_author_vizier(cat):
         Name of the original publication of the catalog (e.g.: "Gaia Collaboration, 2016").
 
     """
-    vizier_catalog = cat.split('/')[:-1]
+    vizier_catalog = cat.split("/")[:-1]
     vizier_name = vizier_catalog[0]
-    for j in range(len(vizier_catalog)-1):
-        vizier_name += '/%s' % vizier_catalog[j + 1]
+    for j in range(len(vizier_catalog) - 1):
+        vizier_name += "/%s" % vizier_catalog[j + 1]
     catalog_list = Vizier.find_catalogs(vizier_name)
     cat_desc = catalog_list[vizier_name].description
     i2 = -1
@@ -128,7 +129,7 @@ def find_author_vizier(cat):
         i2 = cat_desc.find("(", i2 + 1)
         if i2 == -1:
             break
-    cat_ref = cat_desc[cat_desc.find("(", i)+1: cat_desc.find(")", i)]
+    cat_ref = cat_desc[cat_desc.find("(", i) + 1 : cat_desc.find(")", i)]
     return cat_ref
 
 
@@ -136,9 +137,9 @@ def buildVizierRef(sed_name_cat, verbose=False):
     """ Build the Vizier references list. For each search of new SED, check if new catalogs
     are detected and add the reference to the saved json file (default: 'vizier_catalog_naming.json'). 
     """
-    stored_catalog_filepath = store_directory / 'vizier_catalog_references.json'
+    stored_catalog_filepath = store_directory / "vizier_catalog_references.json"
     if Path(stored_catalog_filepath).is_file():
-        with open(stored_catalog_filepath, mode='rt') as ofile:
+        with open(stored_catalog_filepath, mode="rt") as ofile:
             known_entries = json.load(ofile)
     else:
         known_entries = {}
@@ -148,14 +149,16 @@ def buildVizierRef(sed_name_cat, verbose=False):
     not_in_json = [x for x in sed_name_cat_set if x not in known_entries.keys()]
     if len(not_in_json) > 0:
         if verbose:
-            print('\nFetching new Vizier catalog references (%i) from the Vizier database.' % (
-                len(not_in_json)))
+            print(
+                "\nFetching new Vizier catalog references (%i) from the Vizier database."
+                % (len(not_in_json))
+            )
 
         for cat in not_in_json:
             cat_ref = find_author_vizier(cat)
             known_entries[cat] = cat_ref
 
-        with open(stored_catalog_filepath, mode='wt') as ofile:
+        with open(stored_catalog_filepath, mode="wt") as ofile:
             json.dump(known_entries, ofile)
     return known_entries
 
